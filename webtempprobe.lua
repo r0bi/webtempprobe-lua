5cbbe2bda26ab0b5af8bb1b2e2144ae449350133
r0bi <r0bi@users.noreply.github.com>
2016-12-08 15:03:07 -0600

    Update webtempprobe.lua
    the ds18b20 will output 85 degrees Celsius when it is not fully initialized. This addition will delay the web server's response by one second to compensate.

--- webtempprobe.lua
+++ webtempprobe.lua
@@ -8,9 +8,13 @@ srv:listen(80, function(conn)
         print(payload)
         if (ds18b20.read() == nil) then
         	sck:send("HTTP/1.0 200 OK\r\nContent-Type: text/plain\r\n\r\n" .. "Error - Probe not detected")
+        elseif (ds18b20.read() == 85) then
+            print("Waiting for probe to initialize...")
+            tmr.delay(1000000)
+            sck:send("HTTP/1.0 200 OK\r\nContent-Type: text/plain\r\n\r\n" .. "Probe 1|" .. string.format("%.1f", ds18b20.readNumber(nil,'F')))
         else
-        	sck:send("HTTP/1.0 200 OK\r\nContent-Type: text/plain\r\n\r\n" .. "Probe 1|" .. string.format("%.1f", ds18b20.readNumber(nil,'F')))
-    	end
+            sck:send("HTTP/1.0 200 OK\r\nContent-Type: text/plain\r\n\r\n" .. "Probe 1|" .. string.format("%.1f", ds18b20.readNumber(nil,'F')))
+        end
     end)
     conn:on("sent", function(sck) sck:close() end)
 end)
